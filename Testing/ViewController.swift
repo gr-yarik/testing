e54d11849b9d19bd3bdc302ae545356be50d963d
gr-yarik <3rEySq4mG8D8zCD>
2021-03-19 23:06:04 +0200

    add button to screen

--- Testing/ViewController.swift
+++ Testing/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import AuthPM
 
 class ViewController: UIViewController {
 
+    let auth = AuthPM(appId: "sss")
+    let authButton = PMAuthButton(backgroundColor: .red, title: "Войти с помощью PM")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        layoutUI()
         // Do any additional setup after loading the view.
     }
 
+    
+    func layoutUI() {
+        authButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(authButton)
+        NSLayoutConstraint.activate([
+            authButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            authButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            authButton.heightAnchor.constraint(equalToConstant: 100),
+            authButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
+        ])
+    }
 
 }
 
